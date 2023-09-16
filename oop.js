fbd642f32a8f89c498ca5b4fb8ae58fb323aa31d
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-16 23:31:27 +0300

    class

--- oop.js
+++ oop.js
@@ -10,25 +10,49 @@
 //----- class PascalNamedClassName{...}
 
 //2Class Expression:
-const PascalNamedClassName = class {
-  undefinedProperty;
-  extraField = "field-value";
-  //? "new Class" ile obje oluştururken veri göndermek için "constructor" methodu kullanılır.
-  constructor(parameter1, parameter2 = "defeault-value") {
-    this.parameter1=parameter1
-    this.parameter2=parameter2
-  }
-  methodName1() {
-    return this;
-  }
-  methodName2() {
-    return this.extraField;
-  }
-};
+// const PascalNamedClassName = class {
+//   undefinedProperty;
+//   extraField = "field-value";
+//   //? "new Class" ile obje oluştururken veri göndermek için "constructor" methodu kullanılır.
+//   constructor(parameter1, parameter2 = "defeault-value") {
+//     this.parameter1=parameter1
+//     this.parameter2=parameter2
+//   }
+//   methodName1() {
+//     return this;
+//   }
+//   methodName2() {
+//     return this.extraField;
+//   }
+// };
 
-//? Instance
-const instance = new PascalNamedClassName("parameter-1-value", "parameter-2-value");
-console.log(instance);
-console.log(instance.methodName2());
-instance.extraField="new-value"
-console.log(instance.extraField);
+// //? Instance
+// const instance = new PascalNamedClassName("parameter-1-value", "parameter-2-value");
+// console.log(instance);
+// console.log(instance.methodName2());
+// instance.extraField="new-value"
+// console.log(instance.extraField);
+
+
+class Car{
+    isRunning =false
+ constructor(brand, model ,year){
+
+    this.brand=brand
+    this.model=model
+    this.year=year
+ }
+ runEngine(){
+    this.isRunning =true
+    console.log("motor çalıştı")
+    return this.isRunning
+ }
+}
+const ford = new Car ("ford", "Mustng", 1967)
+console.log(ford)
+console.log(ford.isRunning)
+ford.runEngine()
+console.log(ford.isRunning)
+
+const mercedes = new Car ("Mercedes", "CLK200",2000)
+console.log(mercedes)
